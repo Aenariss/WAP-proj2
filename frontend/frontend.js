1a6e234b3a47800bdb2d3aafcd2b247ed17c6859
xgross11 <xgross11@vutbr.cz>
2023-04-17 09:36:09 +0200

    Canvas size bug fix

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -29,8 +29,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let movementOptions = document.getElementById("movements");
 
     // dynamic canvas size based on size of the screen
-    map_canvas.width  = window.innerHeight/1.2;
-    map_canvas.height = window.innerHeight/1.2;
+    map_canvas.width  = window.innerHeight/1.15;
+    map_canvas.height = window.innerHeight/1.15;
 
     let robots = 0;
 
@@ -165,10 +165,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     // listens to resizing of the screen to dynamically resize canvas
     window.addEventListener("resize", function() {
-        map_canvas.width  = window.innerHeight/1.2;
-        map_canvas.height = window.innerHeight/1.2;
-        printMap(map.height, map.width, map_canvas, map_canvas_context, controller);
+        map_canvas.width  = window.innerHeight/1.15;
+        map_canvas.height = window.innerHeight/1.15;
         setCanvasSize(map.width, map.height);
+        printMap(map.height, map.width, map_canvas, map_canvas_context, controller);
     })
     
 })
@@ -219,9 +219,8 @@ function getMapCoordinates(mouseX, mouseY) {
 
 /**
  * Function to compute given mouse click coordinates to column and row indexes
- * @param {int} inputWidht x coordinate of mouse click
- * @param {int} inputHeight y coordinate of mouse click
- * @returns {Object} Object with row and col coordinates of mouse click
+ * @param {int} width width of map inputted by user
+ * @param {int} height height of map inputted by user
  */
 function setCanvasSize(width, height) {
     const cellWidth = map_canvas.width/width;
